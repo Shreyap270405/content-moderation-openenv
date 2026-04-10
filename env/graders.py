a8fced8a78c5f14ddecf174efb09a387a4fd1eb6
Shreyap270405 <shreyap270405@gmail.com>
2026-04-10 14:53:04 +0530

    Add grading function to evaluate predictions

--- env/graders.py
+++ env/graders.py
@@ -1,1 +1,13 @@
+def grade(pred, gt):
+    score = 0.0
 
+    if pred.label == gt["label"]:
+        score += 0.5
+
+    if pred.action == gt["correct_action"]:
+        score += 0.4
+
+    if pred.confidence > 0.7:
+        score += 0.1
+
+    return min(score, 1.0)
