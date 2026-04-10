b2f8c4753467d23f763c65198247401bfb061e4e
Shreyap270405 <shreyap270405@gmail.com>
2026-04-10 14:51:54 +0530

    Update models.py

--- env/models.py
+++ env/models.py
@@ -1,1 +1,13 @@
+from pydantic import BaseModel
+from typing import List
 
+class Observation(BaseModel):
+    content: str
+    sentiment: str
+    user_history_flags: int
+    previous_actions: List[str]
+
+class Action(BaseModel):
+    label: str
+    action: str
+    confidence: float
